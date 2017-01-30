7cc7684473437836552b460a7dbf0e01b52cb8d9
Adam Ryman <adamryman@gmail.com>
2017-01-29 20:03:34 -0800

    Add tmp users table

--- ambition-mysql/createTables.sql
+++ ambition-mysql/createTables.sql
@@ -1,2 +1,3 @@
 CREATE TABLE actions(id SERIAL PRIMARY KEY, action_name varchar(255), user_id integer);
 CREATE TABLE occurrences(id SERIAL PRIMARY KEY, action_id integer, datetime varchar(255), data varchar(255));
+CREATE TABLE users(id SERIAL PRIMARY KEY, username varchar(255), email varchar(255), trello_id varchar(255), trello_webhookurl varchar(255));
