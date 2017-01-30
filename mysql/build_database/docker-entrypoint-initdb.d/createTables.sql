253c10752005630894b59c7edec6a2325983f37c
Adam Ryman <adamryman@gmail.com>
2017-01-29 19:09:41 -0800

    Add ambition-mysql Dockerfile + init; Remove old

--- mysql/build_database/docker-entrypoint-initdb.d/createTables.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE actions(id SERIAL PRIMARY KEY, action_name varchar(255), user_id integer, trello_id varchar(255));
-CREATE TABLE occurrences(id SERIAL PRIMARY KEY, action_id integer, datetime varchar(255), data varchar(255));
