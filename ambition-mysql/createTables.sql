4918e1dae1131bef4442691f2f9462597e1d0213
Adam Ryman <adamryman@gmail.com>
2017-06-25 19:03:38 -0700

    Add sample data to ambition-mysql

--- ambition-mysql/createTables.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS actions(id SERIAL PRIMARY KEY, action_name varchar(255), user_id integer);
-CREATE TABLE IF NOT EXISTS occurrences(id SERIAL PRIMARY KEY, action_id integer, datetime varchar(255), data varchar(255));
-CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username varchar(255), email varchar(255), trello_id varchar(255), trello_webhookurl varchar(255));
